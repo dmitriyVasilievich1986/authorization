322190fe664345a55830b31d22cbf440031eae73
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-06 20:14:54 +0300

    add AccountViewSet

--- account/views.py
+++ account/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .serializer import AccountSerializer
+from .models import Account
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.permissions.AllowAny]
