c498ab68a14d69a1c7ac12c23047cd4129b9a518
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-06 16:32:01 +0300

    update runserver.py - install dotenv

--- runserver.py
+++ runserver.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 from django.core.management import execute_from_command_line
+from dotenv import load_dotenv
 from os import environ
 
-HOST: str = environ.get("HOST", "0.0.0.0")
-PORT: str = environ.get("PORT", "3000")
 
-environ.setdefault('DJANGO_SETTINGS_MODULE', 'authorization.settings')
-execute_from_command_line([__name__, "runserver", f"{HOST}:{PORT}"])
+def runserver(host, port, *args, **kwargs):
+    # make migrations and migrate
+    execute_from_command_line([__name__, "makemigrations"])
+    execute_from_command_line([__name__, "migrate"])
+
+    # start server
+    execute_from_command_line([__name__, "runserver", f"{host}:{port}"])
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    HOST: str = environ.get("HOST", "0.0.0.0")
+    PORT: str = environ.get("PORT", "3000")
+
+    environ.setdefault('DJANGO_SETTINGS_MODULE', 'authorization.settings')
+    runserver(host=HOST, port=PORT)
