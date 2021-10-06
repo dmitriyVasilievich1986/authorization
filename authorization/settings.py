e2544841c7f44275ba64240e9af2451813f2f0ff
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-06 19:49:00 +0300

    setup postgres db

--- authorization/settings.py
+++ authorization/settings.py
@@ -51,14 +51,14 @@ WSGI_APPLICATION = 'authorization.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'PASSWORD': environ.get("DB_PASSWORD", "postgres"),
+        'NAME': environ.get("DB_NAME", "postgres"),
+        'USER': environ.get("DB_USER", "postgres"),
+        'HOST': environ.get("DB_HOST", "db"),
+        'PORT': environ.get("DB_PORT", 5432),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
