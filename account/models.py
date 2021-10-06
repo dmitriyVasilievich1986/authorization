cf7f12556f5cf0f707ee23a307d20c6ab7d8d6cb
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-06 17:00:39 +0300

    add Account model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,19 @@
+from datetime import datetime, timedelta
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    token_expire = models.DateTimeField(blank=False, null=False)
+    email = models.EmailField(verbose_name="email")
+    token = models.CharField(
+        verbose_name="token",
+        max_length=50,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+
+    @staticmethod
+    def get_token_expire_date(*args: list, **kwargs: dict) -> datetime:
+        token_expired: datetime = datetime.now() + timedelta(days=1)
+        return token_expired
