208f5db3fcc0439c7a70916f0f237e3bb78fb043
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-07 11:29:12 +0300

    update tests create second object with expired token check expired token and wrong token cases

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,35 +1,56 @@
-from django.test import TestCase, RequestFactory, client
-from rest_framework.test import APIRequestFactory, APIClient
+from rest_framework.test import APIClient
 from account.models import Account
-from account.views import AccountViewSet
+from django.utils import timezone
+from django.test import TestCase
+from datetime import timedelta
+
+API_URL = "http://localhost/api/accounts/"
 
 
 class AccountModelTest(TestCase):
     def setUp(self) -> None:
-        self.factory = APIRequestFactory()
         self.client = APIClient()
         obj: Account = Account.create(email="test1@gmail.com")
         obj.save()
         self.obj_id = obj.id
         self.token = obj.token
+        obj2: Account = Account.create(email="test2@gmail.com")
+        obj2.token_expire = timezone.now() - timedelta(hours=1)
+        obj2.save()
+        self.second_id = obj2.id
+        self.expired_token = obj2.token
+        self.wrong_id = 22
 
-    def test_create_model(self, *args: tuple, **kwargs: dict) -> None:
-        self.assertEqual(Account.objects.all().count(), 1)
-        obj: Account = Account.create(email="test@gmail.com")
-        obj.save()
-        self.assertEqual(Account.objects.all().count(), 2)
-
-    def test_api_list(self):
-        self.client.credentials(HTTP_AUTHORIZATION='')
-        response = self.client.get('http://localhost/api/accounts/')
+    def test_api_list_wout_credentials(self, *args: tuple, **kwargs: dict) -> None:
+        self.client.credentials(HTTP_AUTHORIZATION='wrong token')
+        response = self.client.get(API_URL)
         self.assertEqual(response.status_code, 404)
-        response = self.client.get('http://localhost/api/accounts/1/')
+        response = self.client.get(f'{API_URL}{self.obj_id}/')
         self.assertEqual(response.status_code, 403)
-        response = self.client.get('http://localhost/api/accounts/2/')
+        response = self.client.get(f'{API_URL}{self.wrong_id}/')
         self.assertEqual(response.status_code, 404)
 
-    def test_api_retrieve(self):
+    def test_api_list_with_credentials(self, *args: tuple, **kwargs: dict) -> None:
         self.client.credentials(HTTP_AUTHORIZATION=f'token {self.token}')
-        url = f'http://localhost/api/accounts/{self.obj_id}/'
+        response = self.client.get(f'{API_URL}')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(f'{API_URL}{self.obj_id}/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(f'{API_URL}{self.second_id}/')
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(f'{API_URL}{self.wrong_id}/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_api_list_with_expired_credentials(self, *args: tuple, **kwargs: dict) -> None:
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'token {self.expired_token}')
+        response = self.client.get(f'{API_URL}')
+        self.assertEqual(response.status_code, 403)
+        response = self.client.get(f'{API_URL}{self.second_id}/')
+        self.assertEqual(response.status_code, 403)
+
+    def test_api_retrieve(self, *args: tuple, **kwargs: dict) -> None:
+        self.client.credentials(HTTP_AUTHORIZATION=f'token {self.token}')
+        url = f'{API_URL}{self.obj_id}/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
