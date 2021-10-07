a1608700e65d2ff763763efdc40ac8ff15eb7c49
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-07 23:32:19 +0300

    change accountViewSet add login method add logout method

--- account/views.py
+++ account/views.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets, permissions, exceptions
+from django.contrib.auth.hashers import check_password
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .serializer import AccountSerializer
 from rest_framework import status
+from django.utils import timezone
 from .models import Account
+import json
 import re
 
 
@@ -42,3 +46,24 @@ class AccountViewSet(viewsets.ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         raise exceptions.NotFound
+
+    @action(methods=["POST"], detail=False)
+    def login(self, request, *args, **kwargs):
+        data: dict = json.loads(request.body)
+        instance: Account = get_object_or_404(Account, email=data.get('email'))
+        if not check_password(data.get("password"), instance.password):
+            raise exceptions.ValidationError({"password": "wrong password."})
+        instance.token_expire = instance.get_token_expire_date()
+        instance.token = instance.generate_token()
+        instance.save()
+        serializer: AccountSerializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    @action(methods=["POST"], detail=False)
+    def logout(self, request, *args, **kwargs):
+        token: str = request.META.get("HTTP_AUTHORIZATION", "")
+        substracted_token: str = re.sub(r'^token ', '', token)
+        instance: Account = get_object_or_404(Account, token=substracted_token)
+        instance.token_expire = timezone.now()
+        instance.save()
+        return Response("logout")
