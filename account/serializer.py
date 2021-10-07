d31cb9b18b467cc4d6d1f694e4c65d810f9d101e
dmitriyVasilievich1986 <dmitriyvasil@gmail.com>
2021-10-07 11:05:35 +0300

    change account viewSet now list function return user if token is exist and active

--- account/serializer.py
+++ account/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from datetime import datetime, timedelta
 from .models import Account
 
 
@@ -8,7 +9,22 @@ class AccountSerializer(ModelSerializer):
         fields = "__all__"
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        instance = Account.create(**validated_data)
+    def create(self, validated_data) -> Account:
+        instance: Account = Account.create(**validated_data)
         instance.save()
         return instance
+
+    def is_same_token(self, token: str) -> bool:
+        is_same: bool = token == f"token {self.data['token']}"
+        return is_same
+
+    @property
+    def token_expire_date(self) -> datetime:
+        date: datetime = datetime.strptime(
+            self.data['token_expire'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        return date
+
+    @property
+    def is_token_expired(self) -> bool:
+        is_expired: bool = self.token_expire_date < datetime.now()
+        return is_expired
--- account/views.py
+++ account/views.py
@@ -1,9 +1,10 @@
-from django.http import response
 from rest_framework import viewsets, permissions, exceptions
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from .serializer import AccountSerializer
 from rest_framework import status
 from .models import Account
+import re
 
 
 class AccountViewSet(viewsets.ModelViewSet):
@@ -12,18 +13,26 @@ class AccountViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
 
     def list(self, request, *args, **kwargs):
-        raise exceptions.NotFound
+        token: str = request.META.get("HTTP_AUTHORIZATION", "")
+        substracted_token: str = re.sub(r'^token ', '', token)
+        instance: Account = get_object_or_404(Account, token=substracted_token)
+        serializer: AccountSerializer = self.get_serializer(instance)
+        if serializer.is_token_expired:
+            raise exceptions.PermissionDenied
+        return Response(serializer.data)
 
     def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        if request.META.get("HTTP_AUTHORIZATION", "") == f"token {serializer.data['token']}":
+        instance: Account = self.get_object()
+        serializer: AccountSerializer = self.get_serializer(instance)
+        token: str = request.META.get("HTTP_AUTHORIZATION", "")
+        if serializer.is_same_token(token) and not serializer.is_token_expired:
             return Response(serializer.data)
         raise exceptions.PermissionDenied
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data, partial=True)
+        serializer: AccountSerializer = self.get_serializer(
+            data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
+        headers: dict = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
